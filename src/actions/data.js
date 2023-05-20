66fda820998748a6d2db3174a7d02206d8f06a0a
Eduardo Duarte <dudu_duarte@hotmail.com>
2023-05-19 21:24:13 -0300

    fex(ecg): fix url

--- src/actions/data.js
+++ src/actions/data.js
@@ -3,7 +3,7 @@ import SET_DATA from './types';
 
 const loadValuesData = async (dispatch) => {
   try {
-    const res = await axios.get('https://cloudecg-env.eba-mau7x2gw.us-east-1.elasticbeanstalk.com/baseR4/Observation/6441a0da0c73d10db306ba36/data/1');
+    const res = await axios.get('http://cloudecg-env.eba-mau7x2gw.us-east-1.elasticbeanstalk.com/baseR4/Observation/6441a0da0c73d10db306ba36/data/1');
     dispatch({
       type: SET_DATA,
       payload: res.data[0].data.split(' '),
