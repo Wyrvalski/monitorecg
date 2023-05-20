0ed46aec6992716e19818e14d3ef0877db6dc6a5
Eduardo Duarte <dudu_duarte@hotmail.com>
2023-05-19 21:05:59 -0300

    fix(ecg): fix lint

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { ThreeCircles } from 'react-loader-spinner';
 import ChartECG from './components';
 import loadValuesData from './actions/data';
-import { Loader, Page, ContainerEcg } from './style';
+import { Loader, Page } from './style';
 
 function App() {
   const [initialValues] = useState(500);
@@ -14,20 +14,6 @@ function App() {
   useEffect(() => {
     dispatch(loadValuesData);
   }, []);
-
-  // const y = [];// this contain the y values (readings) we will post the example values at the end of the article.
-  // const currentPart = 0; // current page is 1st page in the ECG
-  // const baseline = 201; // the is the ECG recording baseline
-  // let total = 0; // total number of pages
-  // let temp = y.length; // calculate the total number of pages
-  // while (temp > 0) {
-  //   temp -= 2000; // as shown in the 1st image , we will draw 4 seconds in each page (each second represented by 500 sample =2000 total sample)
-  //   total++;
-  // }
-  // // initiliazie the graphics object to draw in our Div (i.e. Canvas)
-  // const jg = new jsGraphics('Canvas');
-  // // set this true if we need to allow the user to print it, it will impact performance a little.
-  // jg.setPrintable(true);
 
   return data?.length ? (
     <Page>
