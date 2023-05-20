59dc2220c0449d5f600fb8e4265644ee141d9223
Eduardo Duarte <dudu_duarte@hotmail.com>
2023-05-19 21:38:27 -0300

    fix(ecg): fix url

--- src/actions/data.js
+++ src/actions/data.js
@@ -3,7 +3,16 @@ import SET_DATA from './types';
 
 const loadValuesData = async (dispatch) => {
   try {
-    const res = await axios.get('http://cloudecg-env.eba-mau7x2gw.us-east-1.elasticbeanstalk.com/baseR4/Observation/6441a0da0c73d10db306ba36/data/1');
+    const config = {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Authorization',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    };
+    const res = await axios.get('http://cloudecg-env.eba-mau7x2gw.us-east-1.elasticbeanstalk.com/baseR4/Observation/6441a0da0c73d10db306ba36/data/1', config);
     dispatch({
       type: SET_DATA,
       payload: res.data[0].data.split(' '),
