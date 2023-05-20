a763fe623f678d1b9179594449da5dad291c76e0
Eduardo Duarte <dudu_duarte@hotmail.com>
2023-05-19 22:33:43 -0300

    remove log

--- src/actions/data.js
+++ src/actions/data.js
@@ -18,7 +18,6 @@ const loadValuesData = async (dispatch) => {
       payload: res.data[0].data.split(' '),
     });
   } catch (errr) {
-    console.log(errr, 'https://monitorecg-production.up.railway.app/');
     return errr;
   }
   return null;
